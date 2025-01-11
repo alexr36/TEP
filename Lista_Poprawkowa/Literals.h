f11d2ca3ca5fc86dc42f7521f93fcaf0f192378c
Alex Rogoziński <39859634+alexr36@users.noreply.github.com>
2025-01-11 15:57:19 +0100

    Update Literals.h

--- Lista_Poprawkowa/Literals.h
+++ Lista_Poprawkowa/Literals.h
@@ -4,27 +4,27 @@
 #include "CTimeSpan.h"
 
 
-CTimeSpan operator""_d(unsigned long long timespan_in_days) {
-    return CTimeSpan(timespan_in_days, 0, 0);
+CTimeSpan operator""_d(unsigned long long timespanInDays) {
+    return CTimeSpan(timespanInDays, 0, 0);
 }
 
 
-CTimeSpan operator""_h(unsigned long long timespan_in_hours) {
-    int days = timespan_in_hours / HOURS_IN_A_DAY;
-    timespan_in_hours -= days * HOURS_IN_A_DAY;
+CTimeSpan operator""_h(unsigned long long timespanInHours) {
+    int days = timespanInHours / HOURS_IN_A_DAY;
+    timespanInHours -= days * HOURS_IN_A_DAY;
 
-    return CTimeSpan(days, timespan_in_hours, 0);
+    return CTimeSpan(days, timespanInHours, 0);
 }
 
 
-CTimeSpan operator""_m(unsigned long long timespan_in_minutes) {
-    int hours = timespan_in_minutes / MINUTES_IN_AN_HOUR;
-    timespan_in_minutes -= hours * MINUTES_IN_AN_HOUR;
+CTimeSpan operator""_m(unsigned long long timespanInMinutes) {
+    int hours = timespanInMinutes / MINUTES_IN_AN_HOUR;
+    timespanInMinutes -= hours * MINUTES_IN_AN_HOUR;
 
     int days = hours / HOURS_IN_A_DAY;
     hours -= days * HOURS_IN_A_DAY;
 
-    return CTimeSpan(days, hours, timespan_in_minutes);
+    return CTimeSpan(days, hours, timespanInMinutes);
 }
 
 #endif //LITERALS_H
